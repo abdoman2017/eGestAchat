571b5d20804d910e71140e637c0966dd33d38190
abdoman2017 <33372456+abdoman2017@users.noreply.github.com>
2019-05-29 01:47:30 +0100

    29 05 2019 1h

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -5,9 +5,6 @@
     <title></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar_menu_responsive/navbar_menu.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/fontawesome/css/all.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar_menu_responsive/fontawesome.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/materialize.min.css') }}">
     <script src="{{ asset('js/materialize_0.100/materialize.min.js') }}"></script>
     <script src="{{ asset('js/jquery/jquery.min.js') }}"></script>
